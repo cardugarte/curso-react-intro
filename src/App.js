a5db02d6a8124567d89e5bd9deeaf58635ef76cd
Carlos Dugarte <soycarlosdugarte@gmail.com>
2024-08-31 11:14:03 -0300

    Se hacen pruebas de repositorio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './platzi.webp';
-import './App.css';
+import logo from "./platzi.webp";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+    <TodoItem />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +20,16 @@ function App() {
         </a>
       </header>
     </div>
+  );
+}
+
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Tomar cafe al levantarse</p>
+      <span>X</span>
+    </li>
   );
 }
 
