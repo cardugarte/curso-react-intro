91ef2b0e188b029a58c86e5990dcf3ae58a0a616
Carlos Dugarte <soycarlosdugarte@gmail.com>
2024-09-09 19:49:46 -0300

    add dynamic class CSS

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,7 +3,7 @@ import '../App.css'
 function TodoItem({text, completed}) {
   return (
     <li className='item__container'>
-      <span className='check'>V</span>
+      <span className={`check ${completed && 'completed'}`}>V</span>
       <p className='text'>{text}</p>
       <span className='delete'>X</span>
     </li>
